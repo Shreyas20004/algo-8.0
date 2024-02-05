f5f835c547d25925fedc94f322286458e89758ce
Shreyas20004 <126570047+Shreyas20004@users.noreply.github.com>
2024-02-05 23:21:33 +0530

    Third commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import Logo from '../assets/images/logo.svg'
+import {BsCart2} from "react-icons/bs";
+import {HideOutBars3} from "react-icons/hi2";
+import {Box, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import{
+Box,
+Driver,
+ListItem,
+ListItemButton,
+ListItemIcon,
+ListItemText,}
+from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import RegisterIcon from "@mui/icons-material/AppRegistrationRounded"
+
 
 const Navbar = () => {
-  return (
-    <div>
+  
+        const [OpenMenu, setOpenMenu ] = useState(false);
+        const menuOptions = [
+        {
+            text:"Home",
+            icon:<HomeIcon/>
+        },
+        {
+            text:"About",
+            icon:<InfoIcon/>
+        },
+        {
+            text:"Comments",
+            icon:<CommentRoundedIcon/>
+        },
+        {
+            text:"Register",
+            icon:<RegisterIcon/>
+        },
+    ]
 
-    </div>
-  )
+  return <div>Navbar</div>
 }
 
-export default Navbar
+export default Navbar;
